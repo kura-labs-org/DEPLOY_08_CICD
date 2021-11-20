005cab47fcf133669265b95d4aac10cdcc7a4aaf
Kawang Wong <nycazn724@gmail.com>
2021-11-20 12:20:14 -0500

    Update jenkinsinstall.sh

--- jenkinsinstall.sh
+++ jenkinsinstall.sh
@@ -3,5 +3,6 @@ curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt update
+sudo apt install openjdk-11-jdk
+sudo apt install jenkins
