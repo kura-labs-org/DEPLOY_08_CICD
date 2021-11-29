c33460150801560cd96b726b6f3be74c03c8f089
Zach Cyrus <zscyrus31@gmail.com>
2021-11-29 15:21:40 -0500

    Updated gitignore for python venv

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from os import environ
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 from flask_marshmallow import Marshmallow
@@ -7,7 +8,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\TyronePS\\Desktop\\test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URI')
 
 app.config['SQLALCHEMY_DATABASE_TRACK_MODIFICATIONS'] = False
 
