e76c6b94b0ed971a5e197fa0c9357157b587e5f8
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-11-30 14:49:23 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Hello', () => {
+test('World', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello again/i);
   expect(linkElement).toBeInTheDocument();
