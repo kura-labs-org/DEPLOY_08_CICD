cd0b51db1db8876a5e8f3eeffe7c5cf73bc07c7e
Kawang Wong <nycazn724@gmail.com>
2021-12-03 17:26:34 -0500

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -5,6 +5,6 @@ describe('Heading', () => {
         cy.get('h1')
             .invoke('text')
             .should("equal", "HI, THIS IS THE TEST LINE TO SEE IF THE APP RAN! DO NOT DELETE!")
-    });
+    })
 
-});
+})
