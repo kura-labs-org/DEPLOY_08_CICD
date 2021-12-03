ef360a408c4be065171cd5f439666ae88c99b1d5
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-12-03 18:16:29 -0500

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,6 +1,6 @@
 describe('Title', () => {
     it('Has the right title', () => {
-        cy.visit('http://172.31.88.114:5000/')
+        cy.visit('http://127.0.0.1:3000/')
 
         cy.get('title')
             .invoke('text')
