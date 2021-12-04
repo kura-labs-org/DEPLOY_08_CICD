fb858bda4412d81a3df2f5a55637745651a391d2
Zach Cyrus <zscyrus31@gmail.com>
2021-12-04 18:33:26 -0500

    Changed ip for testing

--- frontend/cypress/integration/0-frontend-app/testing_frontend.spec.js
+++ frontend/cypress/integration/0-frontend-app/testing_frontend.spec.js
@@ -1,6 +1,6 @@
 describe("Testing the Frontend form", () => {
   it("It loads successfully", () => {
-    cy.visit("http://3.87.161.155:3000");
+    cy.visit("http://localhost:3000");
   });
 
   it("Header contains Hello again react", () => {
