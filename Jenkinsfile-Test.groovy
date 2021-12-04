0c5aee5dd2f1fecc9634e4e105a8518863e891d7
Kenneth Tan <89402384+KennethT404@users.noreply.github.com>
2021-12-04 00:37:38 -0500

    Update Jenkinsfile-Test.groovy

--- Jenkinsfile-Test.groovy
+++ Jenkinsfile-Test.groovy
@@ -17,9 +17,9 @@ pipeline {
     stage ('Test') {
       steps {
         sh ''' 
+        sudo apt install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
         npm install cypress
         npm install mocha
-        sudo apt install -y xvfb
         npx cypress run --spec ./cypress/integration/test.spec.js
         npm test
         '''
