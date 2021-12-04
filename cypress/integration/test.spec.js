fda947ffce9caa41ef76a77f4f81c65fef60de83
Ricardo <49110228+Deodutt@users.noreply.github.com>
2021-12-04 18:02:05 -0500

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,10 +1,11 @@
-describe('Heading', () => {
+describe('Title', () => {
     it('has the right title', () => {
-        cy.visit('http://localhost:5000/example-1')
+        // The application is hosted on port 3000
+        cy.visit('http://localhost:3000')
 
         cy.get('h1')
             .invoke('text')
-            .should("equal", "My Awesome Web Application")
+            .should("equal", "Hello again react")
         Cypress.Screenshot.defaults({
         capture: 'runner',
         })
