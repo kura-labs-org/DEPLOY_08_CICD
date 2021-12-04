145709659275fc0024f873a47733f2d6634b4e31
Ricardo <49110228+Deodutt@users.noreply.github.com>
2021-12-04 17:31:30 -0500

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,6 +1,6 @@
 describe('Heading', () => {
     it('has the right title', () => {
-        cy.visit('http://172.31.84.82:5000/example-1')
+        cy.visit('http://localhost:5000/example-1')
 
         cy.get('h1')
             .invoke('text')
