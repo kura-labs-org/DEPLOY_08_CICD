2d3bd133d87ebf8483161537096f8b29271fdd84
Kenneth Tan <89402384+KennethT404@users.noreply.github.com>
2021-12-04 02:01:39 -0500

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,6 +1,6 @@
 describe('Title', () => {
     it('has the right title', () => {
-        cy.visit('http://54.226.56.84:3000')
+        cy.visit('http://127.0.0.0:3000')
 
         cy.get('title')
             .invoke('text')
