10f0264c7d32d65e19a52222977b10b9e1c98b27
afashanu3 <84725239+afashanu3@users.noreply.github.com>
2021-12-03 19:24:59 -0500

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,10 +1,9 @@
-describe('Heading', () => {
-    it('has the right title', () => {
-        cy.visit('http://172.31.31.38:5000/example-1')
+describe('b', () => {
+   it('has the right title', () => {
+      cy.visit('http://18.218.95.240:5000')
 
-        cy.get('h1')
-            .invoke('text')
-            .should("equal", "My Awesome Web Application")
-    });
-
+      cy.get('b')
+          .invoke('text')
+          .should("equal", "Choose you player: 1. LeBron James 2. Michael Jordan 3. Kevin Durant...")
+   });
 });
