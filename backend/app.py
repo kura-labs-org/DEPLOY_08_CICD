494d717d6870e208e202abf2bf54ff8f9e710f3a
Nabeel Shuaib <49292320+TemporarySolace@users.noreply.github.com>
2021-12-04 21:16:42 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\TyronePS\\Desktop\\test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 app.config['SQLALCHEMY_DATABASE_TRACK_MODIFICATIONS'] = False
 
