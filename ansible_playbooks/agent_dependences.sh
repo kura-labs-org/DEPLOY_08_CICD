e89145bb5d921114153ecece20b3d9af61281299
ibrahima <86819806+ibrahima1289@users.noreply.github.com>
2021-12-05 02:15:46 -0500

    Create agent_dependences.sh

---
+++ ansible_playbooks/agent_dependences.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+  sudo apt-get update && sudo apt-get upgrade -y
+  # Install java
+  sudo apt install openjdk-11-jre-headless -y
+  sudo apt-get install -y \
+  default-jre \
+  git \
+  nodejs -y \
+  npm -y 
