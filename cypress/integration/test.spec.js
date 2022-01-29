1f886a4c51eab2288a1de715dddf876b4346dfc9
Andrew Dass <dassandrew3@gmail.com>
2022-01-29 14:17:45 -0500

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,6 +1,6 @@
 describe('Heading', () => {
     it('has the right title', () => {
-        cy.visit('http://172.31.30.41:5000')
+        cy.visit('http://172.31.30.41:5000/example-1')
 
         cy.get('h1')
             .invoke('text')
